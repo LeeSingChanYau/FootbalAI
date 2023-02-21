667bfb0ff64012414aeaaae9d20218c891ea84a3
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-02-21 11:11:49 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+        <h1>Today's matches</h1>
+        <h2>Man City - Man U</h2>
+        <h2>12 PM</h2>
+        <button>Place prediction</button>
+        <h3>Our AI score this game 5 - 0</h3>
+
+        <h1>Today's matches</h1>
+        <h2>Man City - Man U</h2>
+        <h2>12 PM</h2>
+        <button>Place prediction</button>
+        <h3>Our AI score this game 5 - 0</h3>
+
+        <h1>Today's matches</h1>
+        <h2>Man City - Man U</h2>
+        <h2>12 PM</h2>
+        <button>Place prediction</button>
+        <h3>Our AI score this game 5 - 0</h3>
+      </header>
+    </div>
+  );
+}
+
+export default App;
