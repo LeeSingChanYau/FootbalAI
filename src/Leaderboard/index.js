d95288af3d6af845e566194b985e38ae07a47f66
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-02-24 20:36:33 -0800

    added a leaderboard.

--- src/Leaderboard/index.js
+++ src/Leaderboard/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { useState, useEffect } from "react";
-import { getInitialData } from "./data";
-import List from "./List";
-import "./styles.css";
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
 
-const Leaderboard = () => {
-    const [data, setData] = useState(getInitialData());
+import "./styles.css";
+import List from "./List";
+import { getInitialData, genNextData } from "./data";
+
+function App() {
+  const [data, setData] = useState(getInitialData());
   const doSwitch = () => {
     setData(d => {
       [d[0], d[1]] = [d[1], d[0]];
@@ -21,11 +22,11 @@ const Leaderboard = () => {
 
   return (
     <div className="App">
-      <h1>Leaderboard</h1>
       <button onClick={doSwitch}>switch</button>
       <List data={data} />
     </div>
   );
 }
 
-export default Leaderboard;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
