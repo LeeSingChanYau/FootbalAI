3c617a71eb010e23439a68718412054a020e3d5e
Lee Sing Chan Yau <jukerhdchan@gmail.com>
2023-02-28 16:32:46 -0800

    Can fetch from api now.

--- src/Stats/Stats.jsx
+++ src/Stats/Stats.jsx
@@ -1,7 +1,35 @@
 import React from "react";
 import "./Stats.css";
+import { useEffect, useState } from "react";
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '63eec5a4afmsh6264f7af73b4a41p1846c0jsn356a6c62f728',
+		'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+	}
+};
+
+  
 
 const Stats = () => {
+    useEffect(() => {
+        const url = 'https://api-football-v1.p.rapidapi.com/v3/fixtures?league=39&season=2020&from=2021-01-01&to=2021-04-07';
+    
+        const fetchData = async () => {
+          try {
+            const response = await fetch(url, options);
+            const json = await response.json();
+            console.log(json);
+          } catch (error) {
+            console.log("error", error);
+          }
+        };
+    
+        fetchData();
+    }, []);
+
+
     return(
         <div className="stats-body">
         <h1>Today's matches</h1>
